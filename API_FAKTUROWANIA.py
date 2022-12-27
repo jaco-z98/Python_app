0e69aa20aece4bb646b3f69f8a08e6d0e458f481
Jaco <243134@student.pwr.edu.pl>
2022-12-27 19:38:22 +0100

    Api fakturownia

--- API_FAKTUROWANIA.py
+++ API_FAKTUROWANIA.py
@@ -1,22 +1,36 @@
 import requests
 
-lista_faktur = "https://jaco-z.fakturownia.pl/invoices.json?period=this_month&page=1&per_page=25&api_token=7Dlt0jbtcpSJKSqOco"
-
 api_token = "7Dlt0jbtcpSJKSqOco"
 
 headers = { "Authorization": f"Bearer {api_token}" }
 
 def lista_faktur():
-    response = requests.get(lista_faktur, headers=headers)
-    #print(response.status_code)
+    lista = f"https://jaco-z.fakturownia.pl/invoices.json?period=this_month&page=1&per_page=25&api_token={api_token}"
+
+    response = requests.get(lista, headers=headers)
     data = response.json()
     print(data)
 
 def id_faktura():
     id = input("Podaj id:")
-    response = requests.get("https://jaco-z.fakturownia.pl/invoices/191693535.json?api_token=7Dlt0jbtcpSJKSqOco?per_page={id}")
+
+    faktura = f"https://jaco-z.fakturownia.pl/invoices/{id}.json?api_token={api_token}"
+
+    response = requests.get(faktura, headers=headers)
     data = response.json()
     print(data)
 
-lista_faktur()
-id_faktura()
+while True:
+    command = input()
+
+    if command == 'list':
+        lista_faktur()
+        pass
+    elif command == 'byid':
+        id_faktura()
+        pass
+    else:
+        print('No such command.')
+        pass
+
+    pass
